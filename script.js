a5ba912c6e93944dcbd59308bef36252df6ba165
Imbajanox <33042034+Imbajanox@users.noreply.github.com>
2024-10-11 06:51:15 +0200

    Add Modal feature
    Modal that opens when a Image is clicked

--- script.js
+++ script.js
@@ -12,3 +12,47 @@ function closeModal() {
     const modal = document.getElementById("modal");
     modal.style.display = "none";
 }
+
+
+function parseDate(s) {
+		var b = s.split(/\D/);
+		return new Date(b[0],b[1]-1,b[2],b[3],b[4],b[5]);
+	}
+
+    // Funktion zum Auslesen der EXIF-Daten für ein Bild
+    function readExifData(imageId, exifId) {
+        const imgElement = document.getElementById(imageId);
+        EXIF.getData(imgElement, function() {
+            const allExif = EXIF.getAllTags(this);
+			const exposure = 1 / 1/ allExif.ExposureTime;
+
+			const options = {
+				year: "numeric",
+				month: "long",
+				day: "numeric",
+			};
+
+            const exifInfo = `
+			<p><span class="iconify" data-icon="mdi-camera"></span> ${allExif.Make} ${allExif.Model}</p>
+			<p>${allExif.FocalLength}mm</p>
+			<p>f/${allExif.FNumber}</p>
+			<p>1/${exposure}s</p>
+			<p>ISO ${allExif.ISOSpeedRatings}</p>
+			<br>
+			<p>${parseDate(allExif.DateTimeOriginal).toLocaleString('de-DE', options)}</p>`;
+            document.getElementById(exifId).innerHTML = `<strong>Details:</strong> ${exifInfo}`;
+        });
+    }
+
+    // EXIF-Daten für jedes Bild auslesen
+    window.onload = function() {
+        readExifData('img1', 'exif1');
+		readExifData('img2', 'exif2');
+		readExifData('img3', 'exif3');
+		readExifData('img5', 'exif5');
+		readExifData('img6', 'exif6');
+		readExifData('img7', 'exif7');
+		readExifData('img8', 'exif8');
+        // Füge weitere Aufrufe für mehr Bilder hinzu
+		
+    };
